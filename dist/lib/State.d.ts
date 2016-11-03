445616e0073237aefd04fad29d0a2a4374209241
Danny Fritz <dannyfritz@gmail.com>
2016-11-03 06:01:49 -0600

    📇 Remove dist from git

--- dist/lib/State.d.ts
+++
@@ -1,7 +0,0 @@
-export interface State {
-    enter?: Function;
-    pause?: Function;
-    resume?: Function;
-    leave?: Function;
-}
-export declare type StateCallback = "enter" | "pause" | "resume" | "leave";
--- dist/lib/moody-utils.d.ts
+++
@@ -1,9 +0,0 @@
-import { StateCallback } from "./State";
-/**
-    @method callCallback
-    @private
-    @param source {Object} Object bound to `this`
-    @param callbackNAme {String}
-    @param [args] {Array}
-*/
-export declare function callCallback(source: any, callback: StateCallback, args?: IArguments | any[]): any;
